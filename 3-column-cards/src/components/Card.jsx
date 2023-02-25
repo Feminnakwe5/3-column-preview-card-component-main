e87cde92bcaf3007992c9c72b5c0b36863c32ba7
Feminnakwe5 <feminnakwe5@gmail.com>
2023-02-25 14:50:27 -0500

    final edit

--- 3-column-cards/src/components/Card.jsx
+++ 3-column-cards/src/components/Card.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function Card(props) {
-  return <div></div>;
+  return (
+    <div className={props.title}>
+      <img src={props.img} />
+      <h1>{props.title}</h1>
+      <p className='text'> {props.text}</p>
+      <div className='Learn'>Learn More</div>
+    </div>
+  );
 }
 
 export default Card;
